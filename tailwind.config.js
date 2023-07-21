fa67d386f59c213f22ae914ac1610d2d312dc336
emre <emretok35@gmail.com>
2023-07-21 23:45:58 +0300

    base files added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#f27a1a",
+                textColor: "#333333",
+            },
+        },
     },
     plugins: [],
 };
