78b448f7796d967d0714103818f338687026a195
emre <emretok35@gmail.com>
2023-07-22 16:31:09 +0300

    banner rounded

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -6,18 +6,21 @@ export const Banner = () => {
                     <img
                         src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/2/20/Siparisinumut_Tepebanner_200223_202302202219.jpg"
                         alt="asdf"
+                        className="rounded-lg"
                     />
                 </a>
                 <a href="#" className=" w-96">
                     <img
                         src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/2/17/Bakanlik1_202302171954.jpg"
                         alt="asdfdf"
+                        className="rounded-lg"
                     />
                 </a>
                 <a href="#" className=" w-96">
                     <img
                         src="https://cdn.dsmcdn.com/mnresize/500/500/marketing/datascience/automation/2023/4/6/Tobb_Tepebanner_0304231_202304061225.png"
                         alt="asdffs"
+                        className="rounded-lg"
                     />
                 </a>
             </div>
